07f943c6ac14368aa9e485d4d5cefb2fef222f74
Carlos Santana <csantana@us.ibm.com>
2018-01-18 01:29:16 -0500

    add more swift4

--- tests/src/test/scala/actionContainers/Swift4ActionContainerTests.scala
+++ tests/src/test/scala/actionContainers/Swift4ActionContainerTests.scala
@@ -10,7 +10,51 @@ import ActionContainer.withContainer
 @RunWith(classOf[JUnitRunner])
 class Swift4ActionContainerTests extends BasicActionRunnerTests with WskActorSystem {
 
+  val enforceEmptyOutputStream = false
   lazy val swiftContainerImageName = "action-swift-v4"
+  lazy val envCode = makeEnvCode("ProcessInfo.processInfo")
+
+  def makeEnvCode(processInfo: String) = (
+          """
+          |func main(args: [String: Any]) -> [String: Any] {
+          |     let env = """ + processInfo + """.environment
+          |     var a = "???"
+          |     var b = "???"
+          |     var c = "???"
+          |     var d = "???"
+          |     var e = "???"
+          |     var f = "???"
+          |     if let v : String = env["__OW_API_HOST"] {
+          |         a = "\(v)"
+          |     }
+          |     if let v : String = env["__OW_API_KEY"] {
+          |         b = "\(v)"
+          |     }
+          |     if let v : String = env["__OW_NAMESPACE"] {
+          |         c = "\(v)"
+          |     }
+          |     if let v : String = env["__OW_ACTION_NAME"] {
+          |         d = "\(v)"
+          |     }
+          |     if let v : String = env["__OW_ACTIVATION_ID"] {
+          |         e = "\(v)"
+          |     }
+          |     if let v : String = env["__OW_DEADLINE"] {
+          |         f = "\(v)"
+          |     }
+          |     return ["api_host": a, "api_key": b, "namespace": c, "action_name": d, "activation_id": e, "deadline": f]
+          |}
+          """).stripMargin
+  lazy val errorCode = """
+           | // You need an indirection, or swiftc detects the div/0
+           | // at compile-time. Smart.
+           | func div(x: Int, y: Int) -> Int {
+           |     return x/y
+           | }
+           | func main(args: [String: Any]) -> [String: Any] {
+           |     return [ "divBy0": div(x:5, y:0) ]
+           | }
+         """.stripMargin
 
   behavior of swiftContainerImageName
 
@@ -52,6 +96,10 @@ class Swift4ActionContainerTests extends BasicActionRunnerTests with WskActorSys
       """.stripMargin.trim)
   })
 
+  testEnv(Seq {
+    ("swift 4", envCode)
+  }, enforceEmptyOutputStream)
+
 
   it should "support actions using non-default entry points" in {
     withActionContainer() { c =>
@@ -68,6 +116,72 @@ class Swift4ActionContainerTests extends BasicActionRunnerTests with WskActorSys
       runRes.get.fields.get("result") shouldBe Some(JsString("it works"))
     }
   }
+
+  it should "return some error on action error" in {
+    val (out, err) = withActionContainer() { c =>
+      val code = errorCode
+
+      val (initCode, _) = c.init(initPayload(code))
+      initCode should be(200)
+
+      val (runCode, runRes) = c.run(runPayload(JsObject()))
+      runCode should be(502)
+
+      runRes shouldBe defined
+      runRes.get.fields.get("error") shouldBe defined
+    }
+
+    checkStreams(out, err, {
+      case (o, e) =>
+        if (enforceEmptyOutputStream) o shouldBe empty
+        e shouldBe empty
+    })
+  }
+
+  it should "log compilation errors" in {
+    val (out, err) = withActionContainer() { c =>
+      val code = """
+                   | 10 PRINT "Hello!"
+                   | 20 GOTO 10
+                 """.stripMargin
+
+      val (initCode, _) = c.init(initPayload(code))
+      initCode should not be (200)
+    }
+
+    checkStreams(out, err, {
+      case (o, e) =>
+        if (enforceEmptyOutputStream) o shouldBe empty
+        e.toLowerCase should include("error")
+    })
+  }
+
+  it should "support application errors" in {
+    val (out, err) = withActionContainer() { c =>
+      val code = """
+                   | func main(args: [String: Any]) -> [String: Any] {
+                   |     return [ "error": "sorry" ]
+                   | }
+                 """.stripMargin
+
+      val (initCode, _) = c.init(initPayload(code))
+      initCode should be(200)
+
+      val (runCode, runRes) = c.run(runPayload(JsObject()))
+      runCode should be(200) // action writer returning an error is OK
+
+      runRes shouldBe defined
+      runRes should be(Some(JsObject("error" -> JsString("sorry"))))
+    }
+
+    checkStreams(out, err, {
+      case (o, e) =>
+        if (enforceEmptyOutputStream) o shouldBe empty
+        e shouldBe empty
+    })
+  }
+
+
   // Helpers specific to swift actions
   override def withActionContainer(env: Map[String, String] = Map.empty)(code: ActionContainer => Unit) = {
     withContainer(swiftContainerImageName, env)(code)
