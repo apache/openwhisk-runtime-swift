a4782d091a4173804b4aed452df298488f4bc287
Ying Chun Guo <guoyingc@cn.ibm.com>
2018-04-25 02:46:19 +0800

    add license headers (#48)

--- tests/dat/build.sh
+++ tests/dat/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one or more contributor
+# license agreements; and to You under the Apache License, Version 2.0.
+
 set -e
 
 ../../tools/build/compile.sh  HelloSwift3 swift:3.1.1 "-v"
