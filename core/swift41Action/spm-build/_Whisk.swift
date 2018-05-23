ad6b72499879ab9c098f71cc97ce60eb174443a3
Matt Rutkowski <mrutkows@us.ibm.com>
2018-05-23 09:25:21 -0500

    Fix .swift files with incorrect ASF license headers.

--- core/swift41Action/spm-build/_Whisk.swift
+++ core/swift41Action/spm-build/_Whisk.swift
@@ -19,56 +19,56 @@ import Foundation
 import Dispatch
 
 class Whisk {
-    
+
     static var baseUrl = ProcessInfo.processInfo.environment["__OW_API_HOST"]
     static var apiKey = ProcessInfo.processInfo.environment["__OW_API_KEY"]
     // This will allow user to modify the default JSONDecoder and JSONEncoder used by epilogue
     static var jsonDecoder = JSONDecoder()
     static var jsonEncoder = JSONEncoder()
-    
+
     class func invoke(actionNamed action : String, withParameters params : [String:Any], blocking: Bool = true) -> [String:Any] {
         let parsedAction = parseQualifiedName(name: action)
         let strBlocking = blocking ? "true" : "false"
         let path = "/api/v1/namespaces/\(parsedAction.namespace)/actions/\(parsedAction.name)?blocking=\(strBlocking)"
-        
+
         return sendWhiskRequestSyncronish(uriPath: path, params: params, method: "POST")
     }
-    
+
     class func trigger(eventNamed event : String, withParameters params : [String:Any]) -> [String:Any] {
         let parsedEvent = parseQualifiedName(name: event)
         let path = "/api/v1/namespaces/\(parsedEvent.namespace)/triggers/\(parsedEvent.name)?blocking=true"
-        
+
         return sendWhiskRequestSyncronish(uriPath: path, params: params, method: "POST")
     }
-    
+
     class func createTrigger(triggerNamed trigger: String, withParameters params : [String:Any]) -> [String:Any] {
         let parsedTrigger = parseQualifiedName(name: trigger)
         let path = "/api/v1/namespaces/\(parsedTrigger.namespace)/triggers/\(parsedTrigger.name)"
         return sendWhiskRequestSyncronish(uriPath: path, params: params, method: "PUT")
     }
-    
+
     class func createRule(ruleNamed ruleName: String, withTrigger triggerName: String, andAction actionName: String) -> [String:Any] {
         let parsedRule = parseQualifiedName(name: ruleName)
         let path = "/api/v1/namespaces/\(parsedRule.namespace)/rules/\(parsedRule.name)"
         let params = ["trigger":triggerName, "action":actionName]
         return sendWhiskRequestSyncronish(uriPath: path, params: params, method: "PUT")
     }
-    
+
     // handle the GCD dance to make the post async, but then obtain/return
     // the result from this function sync
     private class func sendWhiskRequestSyncronish(uriPath path: String, params : [String:Any], method: String) -> [String:Any] {
         var response : [String:Any]!
-        
+
         let queue = DispatchQueue.global()
         let invokeGroup = DispatchGroup()
-        
+
         invokeGroup.enter()
         queue.async {
             postUrlSession(uriPath: path, params: params, method: method, group: invokeGroup) { result in
                 response = result
             }
         }
-        
+
         // On one hand, FOREVER seems like an awfully long time...
         // But on the other hand, I think we can rely on the system to kill this
         // if it exceeds a reasonable execution time.
@@ -78,46 +78,46 @@ class Whisk {
         case DispatchTimeoutResult.timedOut:
             break
         }
-        
+
         return response
     }
-    
-    
+
+
     /**
      * Using new UrlSession
      */
     private class func postUrlSession(uriPath: String, params : [String:Any], method: String,group: DispatchGroup, callback : @escaping([String:Any]) -> Void) {
-        
+
         guard let encodedPath = uriPath.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) else {
             callback(["error": "Error encoding uri path to make openwhisk REST call."])
             return
         }
-        
+
         let urlStr = "\(baseUrl!)\(encodedPath)"
         if let url = URL(string: urlStr) {
             var request = URLRequest(url: url)
             request.httpMethod = method
-            
+
             do {
                 request.addValue("application/json", forHTTPHeaderField: "Content-Type")
                 request.httpBody = try JSONSerialization.data(withJSONObject: params)
-                
+
                 let loginData: Data = apiKey!.data(using: String.Encoding.utf8, allowLossyConversion: false)!
                 let base64EncodedAuthKey  = loginData.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
                 request.addValue("Basic \(base64EncodedAuthKey)", forHTTPHeaderField: "Authorization")
                 let session = URLSession(configuration: URLSessionConfiguration.default)
-                
+
                 let task = session.dataTask(with: request, completionHandler: {data, response, error -> Void in
-                    
+
                     // exit group after we are done
                     defer {
                         group.leave()
                     }
-                    
+
                     if let error = error {
                         callback(["error":error.localizedDescription])
                     } else {
-                        
+
                         if let data = data {
                             do {
                                 //let outputStr  = String(data: data, encoding: String.Encoding.utf8) as String!
@@ -134,22 +134,22 @@ class Whisk {
                         }
                     }
                 })
-                
+
                 task.resume()
             } catch {
                 callback(["error":"Got error creating params body: \(error)"])
             }
         }
     }
-    
+
     // separate an OpenWhisk qualified name (e.g. "/whisk.system/samples/date")
     // into namespace and name components
     private class func parseQualifiedName(name qualifiedName : String) -> (namespace : String, name : String) {
         let defaultNamespace = "_"
         let delimiter = "/"
-        
+
         let segments :[String] = qualifiedName.components(separatedBy: delimiter)
-        
+
         if segments.count > 2 {
             return (segments[1], Array(segments[2..<segments.count]).joined(separator: delimiter))
         } else if segments.count == 2 {
@@ -160,5 +160,5 @@ class Whisk {
             return (defaultNamespace, segments[0])
         }
     }
-    
+
 }
